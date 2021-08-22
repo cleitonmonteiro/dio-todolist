c1a70ec76fda1cad93dc911734a1c6b8e0a08947
Cleiton Monteiro <cleitonmonteiro.dev@gmail.com>
2021-08-21 22:22:47 -0300

    new task layout and change theme colors

---
+++ app/src/main/java/io/github/cleitonmonteiro/todolist/AddTask.kt
@@ -0,0 +1,11 @@
+package io.github.cleitonmonteiro.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTask : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+    }
+}
